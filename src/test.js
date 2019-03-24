2afdcac1567a8e2b24e75e5e8c4d5a3ff4432c14
Jordan Brown <emagdne@gmail.com>
2019-03-24 00:44:05 -0400

    more nodes in test script

--- src/test.js
+++ src/test.js
@@ -4,7 +4,9 @@ const path = require('path');
 const network = [
     'http://localhost:3000',
     'http://localhost:3001',
-    'http://localhost:3002'
+    'http://localhost:3002',
+    'http://localhost:3003',
+    'http://localhost:3004'
 ].map((x, i) => `${i}:${x}`);
 
 const processes = [];
